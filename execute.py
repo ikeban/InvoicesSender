ec795a8d2f7d7db651535c4bccaa70dd2c9542f4
Ikeban <ikebann@gmail.com>
2020-05-05 22:47:06 +0200

    Added all functionality without sending emails

--- execute.py
+++ execute.py
@@ -3,6 +3,13 @@ import code.ExcelReader as ExcelReader
 import code.TemplateParser as TemplateParser
 import code.PdfAnalyzer as PdfAnalyzer
 
+
+def getFileContent(fileName):
+    read_data = ""
+    with open(fileName) as f:
+        read_data = f.read()
+    return read_data
+
 def main():
     # TODO Do not forget, to remind user, that [MONTH] should be updated before continueing!
     print("If you use [MONTH] in you template, don't forget to update it in InvoiceSenderControl.xlsx")
@@ -17,10 +24,30 @@ def main():
     
     print("Searching pdfs...")
     pdfAnalyzer = PdfAnalyzer.PdfAnalyzer(pdfFileNameToItsContentMap)
+
+    emailContentAttachmentList = []
+
+    for (invoiceText, emailAddress, templateName, keyWordMap) in excelContent:
+        invoicesToAttach = pdfAnalyzer.searchSentenceAndUpdateStats(invoiceText)
+        if len(invoicesToAttach) == 0:
+            print("No invoices for: " + emailAddress + " SKIPPING!")
+            continue  
+        
+        templateContent = getFileContent("emailTemplates/" + templateName)
+        if templateContent is None or templateContent == "":
+            print("template not existing or empty for: " + emailAddress + " SKIPPING!")
+            continue
+        templateParser = TemplateParser.TemplateParser(templateContent, keyWordMap)
+        emailFilledTemplate = templateParser.getFilledTemplate()
+
+        emailContentAttachmentList.append( (emailAddress, emailFilledTemplate, invoicesToAttach) )
+        
     
-    # pdfAnalyzer.searchSentenceAndUpdateStats() # searchedSentence
-    #templateParser = TemplateParser.TemplateParser( ) # templateText, keywordsMap
-    #emailFilledTemplate = templateParser.getFilledTemplate()
+
+
+    print("What will be sent:")
+    for (emailAddress, emailFilledTemplate, invoicesToAttach) in emailContentAttachmentList:
+        print("To " + emailAddress + " will be send " + str(invoicesToAttach))
     
     print("Checking if all PDFs can be delivered:")
     pdfAnalyzer.dropStatistics()
